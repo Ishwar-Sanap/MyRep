ac3c9ccdc7b435ce239d912007925228a07c9e20
Ishwar-Sanap <ishwarsanap512@gmail.com>
2023-04-19 11:10:44 +0530

    added the main.cpp file

---
+++ new.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+int main()
+{   
+    queue<int> q ;
+    vector<int> vt ;
+    vt.push_back(14);
+    cout<<vt[0]<<endl;
+
+    
+    priority_queue<int> pq ;
+    unordered_map<int,int> mp;
+
+    sort(vt.begin() , vt.end());
+    
+    return 0;
+}
